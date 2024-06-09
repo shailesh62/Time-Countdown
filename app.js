94c2f3ba4c1786d43d97b0b5aa553eac5f12df80
Shailesh Kumar Soren <104021118+shailesh62@users.noreply.github.com>
2024-06-09 17:25:06 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,32 @@
+const endDate = "10 June 2024  11:14";
+
+document.getElementById("end-date").innerText = endDate;
+const inputs = document.querySelectorAll("input");
+
+
+const clock = () => {
+    const end = new Date(endDate);
+    const now = new Date();
+    const diff = (end - now)/1000;
+    if(diff < 0) 
+        return;
+
+    inputs[0].value = (Math.floor(diff/60/60/24));
+    inputs[1].value =  (Math.floor(diff/3600)%24); 
+    inputs[2].value = (Math.floor((diff/60)%60)); 
+    inputs[3].value = (Math.floor((diff)%60)); 
+}  
+clock();
+
+setInterval(
+    () => {
+        clock();
+    },1000
+);
+
+
+/*
+// 1 day = 24 hrs
+// 1hr = 60 mins
+// 60 min = 3600 sec
+*/
